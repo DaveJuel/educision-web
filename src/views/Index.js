36fcb68d56f2f3fa12fa91cd4a4a5bf73e1b2c83
david.niwewe <david.niwewe@construction.com>
2025-01-04 10:28:23 +0200

    Added component did mount

--- src/views/Index.js
+++ src/views/Index.js
@@ -11,11 +11,11 @@ import AppNewsletter from "./IndexSections/AppNewsletter.js";
 import AppContactUs from "./IndexSections/AppContactUs.js";
 
 class Reference extends React.Component {
-//   componentDidMount() {
-//     document.documentElement.scrollTop = 0;
-//     document.scrollingElement.scrollTop = 0;
-//     this.refs.main.scrollTop = 0;
-//   }
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
   render() {
     return (
       <>
